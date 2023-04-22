9b499f718a1da01a64b7ddf674df3f15b8b26901
RemiSaurel <r.saurel5@gmail.com>
2023-04-22 19:52:43 +0200

    add: AttractionsPage and DashboardPage

---
+++ src/pages/DashboardPage.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="flex">
+        <vertical-navbar :title="title" :logoUrl="logoUrl"/>
+        <div class="flex ml-8 mt-8 text-3xl font-bold">
+            <img :src="logoUrl" alt="locker" class="h-8 w-8 mr-4" />
+            {{ title }}
+        </div>
+    </div>
+</template>
+
+<script setup lang="ts">
+import VerticalNavbar from "../layouts/VerticalNavbar.vue";
+
+const title = "Dashboard";
+const logoUrl = "src/assets/dashboard.svg";
+</script>
+
+<style scoped>
+</style>
