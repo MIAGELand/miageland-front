4449936c12fce8b1032466c8e223ca2c29a4818d
RemiSaurel <r.saurel5@gmail.com>
2023-05-15 08:42:05 +0200

    fix: french placeholder

--- src/components/datatable/InputSearch.vue
+++ src/components/datatable/InputSearch.vue
@@ -1,6 +1,6 @@
 <template>
     <div>
-        <input type="text" class="py-2 pl-3 pr-10 leading-tight bg-white border border-gray-400 text-gray-800 rounded-xl appearance-none focus:outline-none focus:bg-white focus:border-gray-500" placeholder="Search..." :value="searchText" @input="update($event)">
+        <input type="text" class="py-2 pl-3 pr-10 leading-tight bg-white border border-gray-400 text-gray-800 rounded-xl appearance-none focus:outline-none focus:bg-white focus:border-gray-500" placeholder="Rechercher..." :value="searchText" @input="update($event)">
     </div>
 </template>
 
