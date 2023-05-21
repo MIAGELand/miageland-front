de4ec7ddadb661a40bb11b96e894c1dab8e6300d
RemiSaurel <r.saurel5@gmail.com>
2023-05-22 00:02:03 +0200

    fix: weird colour handler on navbar

--- src/components/connexion/NavigationBar.vue
+++ src/components/connexion/NavigationBar.vue
@@ -7,15 +7,17 @@ defineProps({
 
 <template>
   <div class="border-gray-400 bg-white w-64 rounded-xl p-0.5">
-    <button class="bg-white w-1/2 text-gray-800 p-2 rounded-xl transition-colors ease-in" :class="{'bg-gray-600 text-white' : active === 'visiteur'}" @click="$emit('nav', 'visiteur')">
+    <button class="bg-white w-1/2 text-gray-800 p-2 rounded-xl transition-colors ease-in" :class="{'bg-dark text-white' : active === 'visiteur'}" @click="$emit('nav', 'visiteur')">
       Visiteur
     </button>
-    <button class="bg-white text-gray-800 w-1/2 p-2 rounded-xl transition-colors ease-in" :class="{'bg-gray-600 text-white' : active === 'admin'}" @click="$emit('nav', 'admin')">
+    <button class="bg-white text-gray-800 w-1/2 p-2 rounded-xl transition-colors ease-in" :class="{'bg-dark text-white' : active === 'admin'}" @click="$emit('nav', 'admin')">
       Admin
     </button>
   </div>
 </template>
 
 <style scoped>
-
+.bg-dark {
+    background-color: #1c1c1c;
+}
 </style>
