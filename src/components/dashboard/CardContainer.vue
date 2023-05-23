3f06064484888b411288b4ca11b77b9e488605c5
RemiSaurel <r.saurel5@gmail.com>
2023-05-23 22:13:58 +0200

    add: better ui on cardcontainer dashboard

--- src/components/dashboard/CardContainer.vue
+++ src/components/dashboard/CardContainer.vue
@@ -7,11 +7,13 @@ defineProps({
 
 <template>
   <div
-    class="shadow-2xl bg-slate-600 rounded-lg py-2 h-fit hover:bg-gray-700 transition ease-in-out"
+    class="shadow-2xl bg-slate-600 rounded-lg py-2 h-fit"
   >
-    <div class="flex gap-4 items-center px-4">
-      <span class="text-3xl">{{ emoji }}</span>
-      <span class="text-xl font-bold">{{ title }}</span>
+    <div class="flex items-center justify-between px-4">
+      <div class="flex gap-4 items-center">
+        <span class="text-3xl">{{ emoji }}</span>
+        <span class="text-xl font-bold">{{ title }}</span>
+      </div>
     </div>
     <div class="border border-white"></div>
     <div class="flex flex-col px-4 gap-2">
