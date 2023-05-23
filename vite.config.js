7ab17f9c6f2e276984801d4179008bc438681083
RemiSaurel <r.saurel5@gmail.com>
2023-05-23 11:52:39 +0200

    fix: vite hot reload

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,9 @@
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
+import liveReload from 'vite-plugin-live-reload'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
-});
+    plugins: [vue(), liveReload('./src/**/*.{vue,js,ts,jsx,tsx,css,scss,less,styl}')],
+
+})
